9e8d6c548eb2f04294ee85e79af2a2bb0d470903
SEANDOUGHTY <sean@render.com>
2021-03-29 19:14:34 -0400

    inital commit

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,20 @@
+const HomePage = () => {
+  return (
+    <>
+      <h1>Sample Voting App</h1>
+
+      <div className="voteBox voteLeft">
+        <h3>Dogs</h3>
+        <p>0 Votes (50%)</p>
+        <button>Vote!</button>
+      </div>
+      <div className="voteBox voteRight">
+        <h3>Cats</h3>
+        <p>0 Votes (50%)</p>
+        <button>Vote!</button>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
